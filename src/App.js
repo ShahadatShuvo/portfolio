958d736cda5a0f171de034631eb18a38a074f953
ShahadatShuvo <hossainshuvo7860@gmail.com>
2023-06-07 17:42:30 +0600

    updated

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 
 function App() {
   return (
